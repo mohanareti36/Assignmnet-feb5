86aced2af064822e383a5cca3f1a8d867d6ceeb8
mohanareti36 <mohan.areti@zensar.com>
2021-02-07 19:51:19 +0530

    Add project files.

---
+++ DropDownWeb/CountryDDl.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CountryDDl.aspx.cs" Inherits="DropDownWeb.CountryDDl" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Country,States and City Dropdown list</div>
+        <p>
+            Select Country:<asp:DropDownList ID="ConDDl" runat="server" AppendDataBoundItems="True" AutoPostBack="True" BackColor="#FF9900" Height="20px" OnSelectedIndexChanged="ConDDl_SelectedIndexChanged" Width="150px">
+            </asp:DropDownList>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Select State:<asp:DropDownList ID="StateDDl" runat="server" AppendDataBoundItems="True" AutoPostBack="True" Height="20px" OnSelectedIndexChanged="StateDDl_SelectedIndexChanged" Width="150px">
+            </asp:DropDownList>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            Select City:<asp:DropDownList ID="CityDDl" runat="server" AppendDataBoundItems="True" AutoPostBack="True" BackColor="#66FF33" Height="20px" Width="150px">
+            </asp:DropDownList>
+        </p>
+    </form>
+</body>
+</html>
---
+++ DropDownWeb/CountryDDl.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DropDownWeb
+{
+    public partial class CountryDDl : System.Web.UI.Page
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ToString());
+
+        public void Country()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select country,countryid from country", cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            ConDDl.DataSource = dr;
+            ConDDl.Items.Clear();
+            ConDDl.Items.Add("--Please select country--");
+            ConDDl.DataTextField = "country";
+            ConDDl.DataValueField = "countryid";
+            ConDDl.DataBind();
+            cn.Close();
+        }
+        public void State()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select states,stateid from countrystates where countryid='" + ConDDl.SelectedValue + "'", cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            StateDDl.DataSource = dr;
+            StateDDl.Items.Clear();
+            StateDDl.Items.Add("--Please select the state--");
+            StateDDl.DataTextField = "states";
+            StateDDl.DataValueField = "stateid";
+            StateDDl.DataBind();
+            cn.Close();
+        }
+
+        public void City()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select city,cityid from city where stateid='" + StateDDl.SelectedValue + "'", cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            CityDDl.DataSource = dr;
+            CityDDl.Items.Clear();
+            CityDDl.Items.Add("--please select city--");
+            CityDDl.DataTextField = "city";
+            CityDDl.DataValueField = "cityid";
+            CityDDl.DataBind();
+            cn.Close();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Country();
+            }
+        }
+
+        protected void ConDDl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            State();
+        }
+
+        protected void StateDDl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            City();
+        }
+    }
+}
